aee186e74a9d4f73a058e59e96ee3de3920e3785
Ivan Kalinin <i.kalinin@dexpa.io>
2022-05-27 18:14:36 +0200

    Add equivalent owned public key

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 pub mod ed25519;
 
 #[cfg(feature = "tl-proto")]
@@ -14,5 +16,49 @@ pub mod tl {
         Aes { key: &'tl [u8; 32] },
         #[tl(id = 0xb61f450a)]
         Unencoded { data: &'tl [u8] },
+    }
+
+    impl PublicKey<'_> {
+        pub fn as_equivalent_owned(&self) -> PublicKeyOwned {
+            match self {
+                &Self::Ed25519 { key } => PublicKeyOwned::Ed25519 { key: *key },
+                Self::Overlay { name } => PublicKeyOwned::Overlay {
+                    name: name.to_vec(),
+                },
+                &Self::Aes { key } => PublicKeyOwned::Aes { key: *key },
+                Self::Unencoded { data } => PublicKeyOwned::Unencoded {
+                    data: data.to_vec(),
+                },
+            }
+        }
+    }
+
+    /// Public key which is used in protocol. Owned version
+    #[derive(Debug, Clone, tl_proto::TlRead, tl_proto::TlWrite)]
+    #[tl(boxed)]
+    pub enum PublicKeyOwned {
+        #[tl(id = 0x4813b4c6, size_hint = 32)]
+        Ed25519 { key: [u8; 32] },
+        #[tl(id = 0x34ba45cb)]
+        Overlay { name: Vec<u8> },
+        #[tl(id = 0x2dbcadd4, size_hint = 32)]
+        Aes { key: [u8; 32] },
+        #[tl(id = 0xb61f450a)]
+        Unencoded { data: Vec<u8> },
+    }
+
+    impl PublicKeyOwned {
+        pub fn as_equivalent_ref(&self) -> PublicKey<'_> {
+            match self {
+                Self::Ed25519 { key } => PublicKey::Ed25519 { key },
+                Self::Overlay { name } => PublicKey::Overlay {
+                    name: name.as_slice(),
+                },
+                Self::Aes { key } => PublicKey::Aes { key },
+                Self::Unencoded { data } => PublicKey::Unencoded {
+                    data: data.as_slice(),
+                },
+            }
+        }
     }
 }
